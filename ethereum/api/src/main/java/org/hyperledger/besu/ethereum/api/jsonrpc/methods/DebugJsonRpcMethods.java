bd09dceec524b7e34cb9e9b5a822f3247644b4aa
ayaz-ssvlabs <ayaz@ssvlabs.io>
2026-06-08 20:46:25 +0500

    feat(rpc): implement debug_codeByHash for op-succinct witness preimages
    op-succinct's kona witness generation fetches contract-code preimages
    via debug_codeByHash (supported by op-geth/op-reth); Besu lacked it, so
    witnessgen failed with '-32601 Method not found' ('Error fetching code
    hash preimage'). Add the method: look up bytecode by its hash from the
    Bonsai world-state storage (code-hash storage strategy keys code by
    hash; the account hash is unused for that lookup).

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/DebugJsonRpcMethods.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/DebugJsonRpcMethods.java
@@ -21,6 +21,7 @@ import org.hyperledger.besu.ethereum.api.jsonrpc.internal.DebugReplayBlock;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.DebugAccountAt;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.DebugAccountRange;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.DebugBatchSendRawTransaction;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.DebugCodeByHash;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.DebugGetBadBlocks;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.DebugGetRawBlock;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.DebugGetRawHeader;
@@ -120,6 +121,7 @@ public class DebugJsonRpcMethods extends ApiGroupJsonRpcMethods {
         new DebugGetRawBlock(blockchainQueries),
         new DebugGetRawReceipts(blockchainQueries),
         new DebugGetRawTransaction(blockchainQueries),
+        new DebugCodeByHash(protocolContext.getWorldStateArchive()),
         new DebugTraceCall(
             blockchainQueries,
             protocolSchedule,
